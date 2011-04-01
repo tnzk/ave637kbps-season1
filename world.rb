ecb6609f70d37cfc4bd00602efdad32a04385d44
tnzk <tnzk.marge@gmail.com>
2011-04-01 23:13:41 +0900

    World class now can be modified

--- world.rb
+++ world.rb
@@ -1,5 +1,11 @@
 class World
   def initialize( x = 10, y = 10, z = 10)
-    
+    @chips = Array.new(x){ Array.new(y) { Array.new(z) }}
+  end
+  def [](x,y,z)
+    @chips[x][y][z]
+  end
+  def []=(x,y,z,v)
+    @chips[x][y][z] = v
   end
 end
