348483ffb529c70989c310dfb7f2332b6f33843f
tnzk <tnzk.marge@gmail.com>
2011-04-16 01:07:15 +0900

    Show most under layer

--- hakorb/map_base.rb
+++ hakorb/map_base.rb
@@ -2,6 +2,9 @@ class MapBase
   def initialize( type = :sea)
     @type = type
   end
+  def chip
+    { sea: 2, ground: 0, nothing: 1}[@type]
+  end
   def to_s
     {:sea => '~', :ground => '_', :nothing => '.'}[@type]
   end
