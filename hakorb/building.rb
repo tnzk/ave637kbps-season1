47142893b3c11f6bf6637c241213fee9627d7905
tnzk <tnzk.marge@gmail.com>
2011-06-14 23:47:08 +0900

    Show something like building

--- hakorb/building.rb
+++ hakorb/building.rb
@@ -1,2 +1,6 @@
 class Building < MapBase
+  attr_reader :name
+  def initialize(name)
+    @name = name.to_s ? name : 'default'
+  end
 end
--- hakorb/world.rb
+++ hakorb/world.rb
@@ -1,11 +1,12 @@
 class World
-  attr_reader :width, :height, :depth, :name
+  attr_reader :width, :height, :depth, :name, :blts
   def initialize( x = 10, y = 10, z = 10)
     @name = 'default'
     @width  = x
     @height = y
     @depth  = z
     @chips = Array.new(x){ Array.new(y) { Array.new(z) }}
+    @blts = Array.new(x){ Array.new(y) }
   end
 
   def [](x,y,z)
@@ -38,6 +39,14 @@ class World
             self[x,y,z] = MapBase.new(type)
           end
         end
+      end
+    end
+    open(map_name.sub('map', 'blt')) do |f|
+      field = f.read
+      field.split("\n").each_with_index do |arr, y|
+        arr.split(',').each_with_index { |s, x| 
+          @blts[x][y] = Building.new(s) if s != '0'
+        }
       end
     end
   end
